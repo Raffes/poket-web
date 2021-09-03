2b004e1ebb21024b827724d3c7565a4347a202d5
Raffes <rafaelaperes215@gmail.com>
2021-09-03 13:12:34 -0300

    Adicionado arquivos voltado a atualização dos dados da conta do usuário

---
+++ src/app/users/containers/menu-update-account/menu-update-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-update-account',
+  templateUrl: './menu-update-account.component.html',
+  styleUrls: ['./menu-update-account.component.css']
+})
+export class MenuUpdateAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
