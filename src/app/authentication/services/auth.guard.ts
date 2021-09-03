a372649e38a9515073423b0d666d751e3266c5e9
Raffes <rafaelaperes215@gmail.com>
2021-09-03 13:07:01 -0300

    Adicionado a guarda das rotas dos usuários autenticado

--- src/app/authentication/services/auth.guard.ts
+++ src/app/authentication/services/auth.guard.ts
@@ -1,25 +1,42 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-
+  user: any;
   constructor(
-    public authService: AuthService,
+    public angFireAuth: AngularFireAuth,
     public router: Router
-  ) {}
+  ) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      if(this.authService.isLoggedIn !== false) {
-        this.router.navigate(['sign-in'])
-      }
-    return true;
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return new Promise((resolve, reject) => {
+      this.angFireAuth.onAuthStateChanged((user) => {
+        if (user) {
+
+          // if (!user.emailVerified)                            // if the user hasn't verified their email, send them to that page
+          //     this.router.navigate(['/verify-email']);
+
+          resolve(true);
+        } else {
+          console.log('Auth Guard: user is not logged in');
+          
+          this.router.navigate(['landing-page']);                   // a logged out user will always be sent to home
+          resolve(false);
+        }
+      });
+    });
   }
-  
+
+
 }
+
+
+
