ff11330e26d5d1cdec94f6cc59836fcc3864d3db
Raffes <rafaelaperes215@gmail.com>
2021-09-03 13:07:40 -0300

    Atualizado as funções de autenticação do usuário

--- src/app/authentication/services/auth.service.ts
+++ src/app/authentication/services/auth.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, NgZone } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
-import * as firebase from 'firebase/app';
 import { AlertModalService } from 'src/app/shared/services/alert-modal.service';
 import { User } from '../model/user';
 
@@ -10,41 +9,45 @@ import { User } from '../model/user';
   providedIn: 'root'
 })
 export class AuthService {
+  
+  isLoggedIn: boolean = false;
   userData: any;
 
   constructor(
     public angFireStore: AngularFirestore,
     public angFireAuth: AngularFireAuth,
     public router: Router,
-    public ngZone: NgZone,
     private alertService: AlertModalService
+
   ) {
-      this.angFireAuth.authState.subscribe(user => {
-        if(user) {
+      this.angFireAuth.onAuthStateChanged((user) => {
+        if (user) {
+          this.isLoggedIn = true;
+
           this.userData = user;
           localStorage.setItem('user', JSON.stringify(this.userData));
           JSON.parse(localStorage.getItem('user') || '{}');
-        }else {
-          localStorage.setItem('user', '');
+
+        } else {
+          this.isLoggedIn = false;
+
+          localStorage.setItem('user', '{}');
           JSON.parse(localStorage.getItem('user') || '{}');
+
         }
       })
    }
 
 
-//Sign up with email/password
+// Cadastra usuário pelo email/senha | Sign up with email/password
 SignUp(userData: User) {
   this.angFireAuth.createUserWithEmailAndPassword(userData.email, userData.password)
   .then((result) => {
-    // Call the function sendverficationMail()
-    // this.SendVerificationMail()
-
     
-
+    // Atualizar o nome do usuário | update the user name
     result.user?.updateProfile({
       displayName: userData.displayName
     }).then((resultUser) => {
-      // this.SetUserDataDB(resultUser)
       result.user?.providerData.forEach((profile) => {
         console.log("Sign-in provider: " + profile?.providerId);
         console.log("  Provider-specific UID: " + profile?.uid);
@@ -52,69 +55,37 @@ SignUp(userData: User) {
         console.log("  Email: " + profile?.email);
         console.log("  Photo URL: " + profile?.photoURL);
       });
-      // this.SetUserDataDB(userData, result.user)
     })
 
     this.alertService.showAlertSuccess("Cadastro feito com sucesso :)");
+    return document.location.reload();
     
   }).catch((error) => {
     this.alertService.showAlertDanger("Email ou senha inválidos");
     console.error(error)
   })
-  // return document.location.reload()
+  
 }
 
-
-
-// Sign in with email/password
-SignIn(email: string, password: string) {
+// Loga com email/senha |  Sign in with email/password
+SignIn(email: string, password: string): Promise<any> {
   return this.angFireAuth.signInWithEmailAndPassword(email, password)
-  .then((result) => {
-    this.ngZone.run(() => {
-      this.router.navigate(['dashboard']).then(() => {
-        window.location.reload()
-      })
+  .then(() => {
+
+    console.log('Auth Service: loginUser: success');
+    
+    this.router.navigate(['dashboard']).then(() => {
+      window.location.reload()
     })
-    // this.SetUserDataDB(result.user?.displayName, result.user)
+    
   }).catch((error) => {
     this.alertService.showAlertDanger("Dados não encontrados. Verifique seu email e senha ou crie uma conta");
     console.error(error)
   })
 }
 
-// Sign in with Google
-GoogleAuth() {
-  return this.AuthLogin(new firebase.default.auth.GoogleAuthProvider())
-}
 
-// To run auth providers
-AuthLogin(provider: any) {
-  return this.angFireAuth.signInWithPopup(provider)
-  .then((result) => {
-    this.ngZone.run(() => {
-      this.router.navigate(['dashboard']);
-    })
-    // this.SetUserDataDB(result.user?.displayName, result.user)
-  }).catch((error) => {
-    console.error(error)
-  })
-}
-
-// Send email verfification when new user sign up
-SendVerificationMail() {
-  return this.angFireAuth.currentUser.then(u => u?.sendEmailVerification())
-  .then(() => {
-    this.router.navigate(['verify-email-address'])
-  })
-}
-
-// Returns true when user is looged in and email is verified
-get isLoggedIn(): boolean {
-  const user = JSON.parse(localStorage.getItem('user') || '')
-  return (user != null && user.emailVerified !== false) ? true : false
-}
-
-// Send email when user Forgot password
+// Manda email quando o usuário esquece a senha | Send email when user Forgot password
 ForgotPassword(passwordResetEmail: string) {
   return this.angFireAuth.sendPasswordResetEmail(passwordResetEmail)
   .then(() => {
@@ -125,32 +96,13 @@ ForgotPassword(passwordResetEmail: string) {
   })) 
 }
 
-/* Setting up user data when sign in with username/password, 
-  sign up with username/password and sign in with social auth  
-  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
-SetUserDataDB(userPwd: any, userAuth: any) {
-  const userRef: AngularFirestoreDocument<any> = this.angFireStore.doc(`users/${userAuth.uid}`)
-  const userData: User = {
-    uid: userAuth.uid,
-    email: userAuth.email,
-    displayName: userAuth.displayName,
-    photoURL: userAuth.photoURL,
-    emailVerified: userAuth.emailVerified,
-    password: userPwd.password
-  }
 
-  return userRef.set(userData, {
-    merge: true
-  })
-}
-
-// Sign out
+// Sair da autenticação do usuário | Sign out
 SignOut() {
   return this.angFireAuth.signOut().then(() => {
     localStorage.removeItem('user');
-    this.router.navigate(['landing-page']).then(() => {
-      document.location.reload()
-    })
+
+    this.router.navigate(['landing-page'])
   })
 }
 
