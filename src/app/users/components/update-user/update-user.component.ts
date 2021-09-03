b83ecf4b290f03223e67ac46af4dd72b9b6c2199
Raffes <rafaelaperes215@gmail.com>
2021-09-03 17:30:32 -0300

    Adicionado arquivo e funcionalidade de atualizar os dados do usuário

--- src/app/users/components/update-user/update-user.component.ts
+++ src/app/users/components/update-user/update-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/authentication/services/auth.service';
+import { AlertModalService } from 'src/app/shared/services/alert-modal.service';
 
 @Component({
   selector: 'app-update-user',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateUserComponent implements OnInit {
 
-  constructor() { }
+  public editUserInformation: FormGroup;
+
+  constructor(
+    public authService: AuthService,
+    public formBuilder: FormBuilder,
+    private alertService: AlertModalService
+  ) {
+      this.editUserInformation = this.formBuilder.group({
+        displayName: ["", Validators.maxLength(30)],
+        email: ["", Validators.maxLength(30)]
+      })
+   }
 
   ngOnInit(): void {
+    this.editUserInformation = this.formBuilder.group({
+        displayName: [this.authService.userData.displayName],
+        email: [this.authService.userData.email]
+    })
+  }
+
+
+  onSubmit(){
+    const name = this.editUserInformation.get('displayName')
+    const email = this.editUserInformation.get('email')
+
+      if(name?.value == "" || email?.value == ""){
+        this.alertService.showAlertDanger("Falta campos para preencher");
+
+      }else {
+        this.authService.updateUserEmailNickname(this.editUserInformation.value)
+      }
   }
 
 }
