efeea983c83608c4a30d91127217bad46192a354
Raffes <rafaelaperes215@gmail.com>
2021-09-03 17:29:33 -0300

    Adicionado arquivo e funcionalidade de atualizar os dados do usuário

--- src/app/users/components/update-password/update-password.component.ts
+++ src/app/users/components/update-password/update-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/authentication/services/auth.service';
+import { AlertModalService } from 'src/app/shared/services/alert-modal.service';
 
 @Component({
   selector: 'app-update-password',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-password.component.css']
 })
 export class UpdatePasswordComponent implements OnInit {
+  
+  public editUserPassword: FormGroup;
 
-  constructor() { }
+  constructor(
+    public authService: AuthService,
+    public formBuilder: FormBuilder,
+    private alertService: AlertModalService
+  ) { 
+    this.editUserPassword = this.formBuilder.group({
+      displayName: [""],
+      emailUpdateUser: [""]
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit(){}
 
 }
