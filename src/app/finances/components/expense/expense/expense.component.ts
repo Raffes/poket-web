e28e1ed0b811bdd1e41038b93ea04d36c445adb9
Raffes <rafaelaperes215@gmail.com>
2021-11-09 15:48:08 -0300

    Adicionado a funcionalidade de identificar se tem conta cadastrada, caso nao ele aparece um botao para cadastrar alguma conta

--- src/app/finances/components/expense/expense/expense.component.ts
+++ src/app/finances/components/expense/expense/expense.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder } from '@angular/forms';
 import { AuthService } from 'src/app/authentication/services/auth.service';
 import { ExpenseCrudModalService } from 'src/app/finances/services/expense-crud-modal.service';
 import { ExpenseService } from 'src/app/finances/services/expense.service';
+import { ModalWalletCrudService } from 'src/app/finances/services/wallet-crud-modal.service';
+import { WalletService } from 'src/app/finances/services/wallet.service';
 
 @Component({
   selector: 'app-expense',
@@ -12,16 +14,21 @@ import { ExpenseService } from 'src/app/finances/services/expense.service';
 export class ExpenseComponent implements OnInit {
 
   valor: any
+
+  Wallet: any;
   
   constructor(
     public expenseService: ExpenseService,
     public modalExpenseService: ExpenseCrudModalService,
     public authService: AuthService,
+    public walletService: WalletService,
+    public ModalWalletService: ModalWalletCrudService,
     public formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.totalExpense()
+    this.listWallet()
   }
 
   totalExpense() {
@@ -44,6 +51,22 @@ export class ExpenseComponent implements OnInit {
   modalCreateExpense() {
     this.modalExpenseService.showCreateExpense()
 
+  }
+
+  listWallet() {
+    this.walletService.getWalletList(this.authService.userData.uid).subscribe(res => {
+      this.Wallet = res.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as unknown;
+      })
+
+    })
+  }
+
+  modalCreateWallet() {
+    this.ModalWalletService.showCreateWallet()
   }
 
 }
