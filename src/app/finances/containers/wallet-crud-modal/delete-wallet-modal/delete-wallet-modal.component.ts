9a2355965874411da34318906630a982f6aa799e
Raffes <rafaelaperes215@gmail.com>
2021-11-09 21:44:12 -0300

    Adicionado a funcionadde de excluir renda, despesa e planejamento, caso a conta associado a eles apaguem

--- src/app/finances/containers/wallet-crud-modal/delete-wallet-modal/delete-wallet-modal.component.ts
+++ src/app/finances/containers/wallet-crud-modal/delete-wallet-modal/delete-wallet-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { AuthService } from 'src/app/authentication/services/auth.service';
+import { ExpenseService } from 'src/app/finances/services/expense.service';
+import { FinancialPlanningService } from 'src/app/finances/services/financial-planning.service';
+import { IncomeService } from 'src/app/finances/services/income.service';
 import { WalletService } from 'src/app/finances/services/wallet.service';
 
 @Component({
@@ -15,6 +18,9 @@ export class DeleteWalletModalComponent implements OnInit {
   @Input() valor: any;
 
   constructor(
+    public incomeService: IncomeService,
+    public expenseService: ExpenseService,
+    public fpService: FinancialPlanningService,
     public bsModalRef: BsModalRef,
     public walletService: WalletService,
     public authService: AuthService
@@ -27,9 +33,94 @@ export class DeleteWalletModalComponent implements OnInit {
     this.bsModalRef.hide();
   }
 
-  deleteWallet(){
-      this.walletService.deleteWallet(this.authService.userData.uid, this.id)
-      this.closeModal()
+  deleteWallet() {
+
+
+    // Apaga as rendas associada a essa conta
+    this.incomeService.getIncomeList(this.authService.userData.uid).subscribe(res => {
+
+      let allIncome = res.map(el => {
+        return {
+          id: el.payload.doc.id,
+          idConta: el.payload.doc.data().idConta
+        }
+
+      })
+
+      let idAllIncome = allIncome.filter(el => el.idConta === this.id)
+
+      idAllIncome.forEach((el, i) => {
+        this.incomeService.deleteIncomeAfterWallet(this.authService.userData.uid, idAllIncome[i].id)
+      })
+
+
+    })
+
+    // Apaga as despesas associada a essa conta
+    this.expenseService.getExpenseList(this.authService.userData.uid).subscribe(res => {
+
+      let allExpense = res.map(el => {
+        return {
+          id: el.payload.doc.id,
+          idConta: el.payload.doc.data().idConta
+        }
+
+      })
+
+      let idAllExpense = allExpense.filter(el => el.idConta == this.id)
+
+      idAllExpense.forEach((el, i) => {
+
+        this.expenseService.deleteExpenseAfterWallet(this.authService.userData.uid, idAllExpense[i].id)
+      })
+
+
+
+    })
+
+    // Apaga as planejamentos financeiro associada a essa conta
+    this.fpService.getFinancialPlanningList(this.authService.userData.uid).subscribe(res => {
+
+      let allFp = res.map(el => {
+        return {
+          id: el.payload.doc.id,
+          idConta: el.payload.doc.data().idConta
+        }
+
+      })
+
+      let idAllFp = allFp.filter(el => el.idConta == this.id)
+
+      idAllFp.forEach((el, i) => {
+        // Apaga as o histórico associado ao planejamento
+        this.fpService.getHistoryFinancialPlanningList(this.authService.userData.uid, idAllFp[i].id).subscribe(res => {
+
+          let allFpHistory = res.map(el => {
+            return {
+              id: el.payload.doc.id,
+              idConta: el.payload.doc.data().idConta
+            }
+
+          })
+
+          let idAllFpHistory = allFpHistory.filter(el => el.idConta == this.id)
+
+          idAllFpHistory.forEach((el2, i2) => {
+            this.fpService.deleteHistoryFpAfterWallet(this.authService.userData.uid, idAllFp[i].id, idAllFpHistory[i2].id)
+            this.fpService.deleteFinancialPlanning(this.authService.userData.uid, idAllFp[i].id)
+          })
+
+
+        })
+      })
+
+    })
+
+    // Apaga conta financeira do sistema
+    this.walletService.deleteWallet(this.authService.userData.uid, this.id)
+
+
+    this.closeModal()
 
   }
 
